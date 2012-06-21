11a660a13873e39b6efc090954653dc5ab22c3fa
blackbing <blackbing@gmail.com>
2012-06-21 07:04:03 -0400

    backbone selector

--- coffee/photoBoxView.coffee
+++ coffee/photoBoxView.coffee
@@ -3,28 +3,28 @@ define [
   'text!../template/photoBox.hbs'
 ], (module, photoBoxTpl)->
   _selfPrefix = module.id
-  relayout = _.debounce((event) ->
-    if $("#photoBox").css("visibility") is "hidden"
-      return
-    delta = 10
-
-    descWidth = (if $(".pb-desc").is(":hidden") then 0 else $(".pb-desc").width())
-    viewerWidth = $("body").width() - descWidth - (20 * 2) - 30
-    viewerHeight = $("body").height() - (20 * 2) - $(".pb-preview").height()
-    $(".pb-viewer").height viewerHeight
-    $(".pb-layer").width viewerWidth
-    maxWidth = viewerWidth
-    $(".pb-main-image img").css(
-      maxWidth: viewerWidth - delta
-      maxHeight: viewerHeight - 20
-    ).trigger "load"
-    listWidth = $("body").width() - (50 * 2)
-    $(".pb-list-wrapper").width listWidth
-    $list = $("#photoBox .pb-list li")
-    $("#photoBox .pb-list").width $list.length * ($list.width() + 4 * 2)
-  , 200)
 
   PhotoBoxView = Backbone.View.extend(
+    relayout: ->
+      if @$("#photoBox").css("visibility") is "hidden"
+        return
+      delta = 10
+
+      descWidth = (if @$(".pb-desc").is(":hidden") then 0 else @$(".pb-desc").width())
+      viewerWidth = $("body").width() - descWidth - (20 * 2) - 30
+      viewerHeight = $("body").height() - (20 * 2) - @$(".pb-preview").height()
+      @$(".pb-viewer").height viewerHeight
+      @$(".pb-layer").width viewerWidth
+      maxWidth = viewerWidth
+      @$(".pb-main-image img").css(
+        maxWidth: viewerWidth - delta
+        maxHeight: viewerHeight - 20
+      ).trigger "load"
+      listWidth = $("body").width() - (50 * 2)
+      @$(".pb-list-wrapper").width listWidth
+      $list = @$("#photoBox .pb-list li")
+      @$("#photoBox .pb-list").width $list.length * ($list.width() + 4 * 2)
+
     events:
       'click .pb-list li': 'selectPhoto'
       'click .pb-close': 'close'
@@ -32,20 +32,20 @@ define [
       "click .pb-right-handler": 'next'
 
     next: (event)->
-      $list = $("#photoBox .pb-list")
+      $list = @$("#photoBox .pb-list")
       list_left = parseInt($list.css("left"), 10)
-      min = -($("#photoBox .pb-list").width() - $(".pb-list-wrapper").width())
+      min = -(@$("#photoBox .pb-list").width() - @$(".pb-list-wrapper").width())
       remain = Math.abs(list_left - min)
       if remain <= 0
         return
       else
         left = Math.min(400, remain)
-        $("#photoBox .pb-list").animate
+        @$("#photoBox .pb-list").animate
           left: "-=" + left
         , 200
 
     previous: (event)->
-      $list = $("#photoBox .pb-list")
+      $list = @$("#photoBox .pb-list")
       list_left = parseInt($list.css("left"), 10)
       min = 0
       remain = Math.abs(list_left - min)
@@ -53,46 +53,46 @@ define [
         return
       else
         left = Math.min(400, remain)
-        $("#photoBox .pb-list").animate
+        @$("#photoBox .pb-list").animate
           left: "+=" + left
         , 200
 
     open: (event)->
-      @$el.find("#photoBoxShadow")
+      @$("#photoBoxShadow")
       .attr('class', 'photoBox open')
       .one('webkitTransitionEnd', (event)=>
         _.delay(=>
-          @$el.find('#photoBox').removeClass('hidden')
-          relayout()
-          @$el.find("#photoBoxShadow").addClass('hidden')
+          @$('#photoBox').removeClass('hidden')
+          @relayout()
+          @$("#photoBoxShadow").addClass('hidden')
         , 200)
       )
 
 
     close: (event)->
-      @$el.find("#photoBox").addClass "hidden"
+      @$("#photoBox").addClass "hidden"
 
-      @$el.find("#photoBoxShadow")
+      @$("#photoBoxShadow")
       .removeClass('hidden open')
       .addClass('close')
       .one('webkitTransitionEnd', (event)=>
         _.delay(=>
-          @$el.find('#photoBox').addClass('hidden')
-          @$el.find("#photoBoxShadow").addClass('hidden')
+          @$('#photoBox').addClass('hidden')
+          @$("#photoBoxShadow").addClass('hidden')
         , 200)
       )
 
     selectPhoto: (event)->
       $target = $(event.currentTarget)
 
-      @$el.find(".pb-list>li>a").removeClass("active")
+      @$(".pb-list>li>a").removeClass("active")
       $target.find("a").addClass("active")
       bkImgReg = /url\((.+)\)/
       bkImg = bkImgReg.exec($target.css("background-image"))
       if bkImg
         img = new Image()
         img.onload = ->
-          $(".pb-main-image img").attr "src", @src
+          @$(".pb-main-image img").attr "src", @src
 
         img.src = bkImg[1]
 
@@ -106,11 +106,11 @@ define [
 
       $photoBox = @$el
       #$photoBox.after "<div class=\"photoBox hidden\" id=\"photoBoxShadow\"/>"
-      $photoBox.find(".pb-list").draggable
+      @$(".pb-list").draggable
         axis: "x"
-        drag: (event, ui) ->
+        drag: (event, ui) =>
           left = ui.position.left
-          min = -($photoBox.find(".pb-list").width() - $photoBox.find(".pb-list-wrapper").width())
+          min = -(@$(".pb-list").width() - @$(".pb-list-wrapper").width())
           max = 0
           false  if (left - max) * (left - min) > 0
 
@@ -118,8 +118,12 @@ define [
       @render()
 
 
-      $(window).resize relayout
-      relayout()
+      $(window).resize(
+        _.debounce( =>
+          @relayout()
+        , 200)
+      )
+      @relayout()
 
 
   )
--- javascript/photoBoxView.js
+++ javascript/photoBoxView.js
@@ -1,28 +1,28 @@
 (function() {
 
   define(['module', 'text!../template/photoBox.hbs'], function(module, photoBoxTpl) {
-    var PhotoBoxView, relayout, _selfPrefix;
+    var PhotoBoxView, _selfPrefix;
     _selfPrefix = module.id;
-    relayout = _.debounce(function(event) {
-      var $list, delta, descWidth, listWidth, maxWidth, viewerHeight, viewerWidth;
-      if ($("#photoBox").css("visibility") === "hidden") return;
-      delta = 10;
-      descWidth = ($(".pb-desc").is(":hidden") ? 0 : $(".pb-desc").width());
-      viewerWidth = $("body").width() - descWidth - (20 * 2) - 30;
-      viewerHeight = $("body").height() - (20 * 2) - $(".pb-preview").height();
-      $(".pb-viewer").height(viewerHeight);
-      $(".pb-layer").width(viewerWidth);
-      maxWidth = viewerWidth;
-      $(".pb-main-image img").css({
-        maxWidth: viewerWidth - delta,
-        maxHeight: viewerHeight - 20
-      }).trigger("load");
-      listWidth = $("body").width() - (50 * 2);
-      $(".pb-list-wrapper").width(listWidth);
-      $list = $("#photoBox .pb-list li");
-      return $("#photoBox .pb-list").width($list.length * ($list.width() + 4 * 2));
-    }, 200);
     PhotoBoxView = Backbone.View.extend({
+      relayout: function() {
+        var $list, delta, descWidth, listWidth, maxWidth, viewerHeight, viewerWidth;
+        if (this.$("#photoBox").css("visibility") === "hidden") return;
+        delta = 10;
+        descWidth = (this.$(".pb-desc").is(":hidden") ? 0 : this.$(".pb-desc").width());
+        viewerWidth = $("body").width() - descWidth - (20 * 2) - 30;
+        viewerHeight = $("body").height() - (20 * 2) - this.$(".pb-preview").height();
+        this.$(".pb-viewer").height(viewerHeight);
+        this.$(".pb-layer").width(viewerWidth);
+        maxWidth = viewerWidth;
+        this.$(".pb-main-image img").css({
+          maxWidth: viewerWidth - delta,
+          maxHeight: viewerHeight - 20
+        }).trigger("load");
+        listWidth = $("body").width() - (50 * 2);
+        this.$(".pb-list-wrapper").width(listWidth);
+        $list = this.$("#photoBox .pb-list li");
+        return this.$("#photoBox .pb-list").width($list.length * ($list.width() + 4 * 2));
+      },
       events: {
         'click .pb-list li': 'selectPhoto',
         'click .pb-close': 'close',
@@ -31,86 +31,90 @@
       },
       next: function(event) {
         var $list, left, list_left, min, remain;
-        $list = $("#photoBox .pb-list");
+        $list = this.$("#photoBox .pb-list");
         list_left = parseInt($list.css("left"), 10);
-        min = -($("#photoBox .pb-list").width() - $(".pb-list-wrapper").width());
+        min = -(this.$("#photoBox .pb-list").width() - this.$(".pb-list-wrapper").width());
         remain = Math.abs(list_left - min);
         if (remain <= 0) {} else {
           left = Math.min(400, remain);
-          return $("#photoBox .pb-list").animate({
+          return this.$("#photoBox .pb-list").animate({
             left: "-=" + left
           }, 200);
         }
       },
       previous: function(event) {
         var $list, left, list_left, min, remain;
-        $list = $("#photoBox .pb-list");
+        $list = this.$("#photoBox .pb-list");
         list_left = parseInt($list.css("left"), 10);
         min = 0;
         remain = Math.abs(list_left - min);
         if (remain <= 0) {} else {
           left = Math.min(400, remain);
-          return $("#photoBox .pb-list").animate({
+          return this.$("#photoBox .pb-list").animate({
             left: "+=" + left
           }, 200);
         }
       },
       open: function(event) {
         var _this = this;
-        return this.$el.find("#photoBoxShadow").attr('class', 'photoBox open').one('webkitTransitionEnd', function(event) {
+        return this.$("#photoBoxShadow").attr('class', 'photoBox open').one('webkitTransitionEnd', function(event) {
           return _.delay(function() {
-            _this.$el.find('#photoBox').removeClass('hidden');
-            relayout();
-            return _this.$el.find("#photoBoxShadow").addClass('hidden');
+            _this.$('#photoBox').removeClass('hidden');
+            _this.relayout();
+            return _this.$("#photoBoxShadow").addClass('hidden');
           }, 200);
         });
       },
       close: function(event) {
         var _this = this;
-        this.$el.find("#photoBox").addClass("hidden");
-        return this.$el.find("#photoBoxShadow").removeClass('hidden open').addClass('close').one('webkitTransitionEnd', function(event) {
+        this.$("#photoBox").addClass("hidden");
+        return this.$("#photoBoxShadow").removeClass('hidden open').addClass('close').one('webkitTransitionEnd', function(event) {
           return _.delay(function() {
-            _this.$el.find('#photoBox').addClass('hidden');
-            return _this.$el.find("#photoBoxShadow").addClass('hidden');
+            _this.$('#photoBox').addClass('hidden');
+            return _this.$("#photoBoxShadow").addClass('hidden');
           }, 200);
         });
       },
       selectPhoto: function(event) {
         var $target, bkImg, bkImgReg, img;
         $target = $(event.currentTarget);
-        this.$el.find(".pb-list>li>a").removeClass("active");
+        this.$(".pb-list>li>a").removeClass("active");
         $target.find("a").addClass("active");
         bkImgReg = /url\((.+)\)/;
         bkImg = bkImgReg.exec($target.css("background-image"));
         if (bkImg) {
           img = new Image();
           img.onload = function() {
-            return $(".pb-main-image img").attr("src", this.src);
+            return this.$(".pb-main-image img").attr("src", this.src);
           };
           return img.src = bkImg[1];
         }
       },
       render: function() {
-        var $photoBox, data, template;
+        var $photoBox, data, template,
+          _this = this;
         template = Handlebars.compile(photoBoxTpl);
         data = {};
         this.$el.append(template(data));
         $photoBox = this.$el;
-        return $photoBox.find(".pb-list").draggable({
+        return this.$(".pb-list").draggable({
           axis: "x",
           drag: function(event, ui) {
             var left, max, min;
             left = ui.position.left;
-            min = -($photoBox.find(".pb-list").width() - $photoBox.find(".pb-list-wrapper").width());
+            min = -(_this.$(".pb-list").width() - _this.$(".pb-list-wrapper").width());
             max = 0;
             if ((left - max) * (left - min) > 0) return false;
           }
         });
       },
       initialize: function() {
+        var _this = this;
         this.render();
-        $(window).resize(relayout);
-        return relayout();
+        $(window).resize(_.debounce(function() {
+          return _this.relayout();
+        }, 200));
+        return this.relayout();
       }
     });
     return PhotoBoxView;
